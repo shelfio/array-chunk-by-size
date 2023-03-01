6635798303e6c1d4c6cb03869033da22ab421e6b
harazdovskiy <harazdovskiy@gmail.com>
2023-03-02 01:36:58 +0200

    feat: Added TS Support

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,11 @@
-const {chunkArray} = require('.');
+import {chunkArray} from './index';
 
-function getObjWithSize(times) {
-  return {abcd: 'a'.repeat(times - 11)}
+function getObjWithSize(times: number) {
+  return {abcd: 'a'.repeat(times - 11)};
 }
 
-function getObjWithSizePoop(times) {
-  return {abcd: '💩'.repeat(times - 1)}
+function getObjWithSizePoop(times: number) {
+  return {abcd: '💩'.repeat(times - 1)};
 }
 
 it('should export chunkArray function', () => {
@@ -13,97 +13,91 @@ it('should export chunkArray function', () => {
 });
 
 it('should return empty array if empty passed', () => {
-  const input = [];
-  const output = chunkArray({input});
+  const output = chunkArray({input: []});
+
   expect(output).toEqual([]);
 });
 
 it('should return empty array if size is invalid', () => {
   const input = [{a: 1}];
   const output = chunkArray({input, bytesSize: -100});
+
   expect(output).toEqual([]);
 });
 
 it('should split 3 equal items into 2 chunks 2+1', () => {
-  const input = [
-    getObjWithSize(100), getObjWithSize(100),
-    getObjWithSize(100)
-  ];
+  const input = [getObjWithSize(100), getObjWithSize(100), getObjWithSize(100)];
   const output = chunkArray({input, bytesSize: 200});
 
-  expect(output).toEqual([
-    [getObjWithSize(100), getObjWithSize(100)],
-    [getObjWithSize(100)]
-  ]);
+  expect(output).toEqual([[getObjWithSize(100), getObjWithSize(100)], [getObjWithSize(100)]]);
 });
 
 it('should split 3 equal items into 2 chunks 2+1 if not 100 but 25 symbols of 4 bytes', () => {
-  const input = [
-    getObjWithSizePoop(23), getObjWithSizePoop(23),
-    getObjWithSizePoop(23)
-  ];
+  const input = [getObjWithSizePoop(23), getObjWithSizePoop(23), getObjWithSizePoop(23)];
   const output = chunkArray({input, bytesSize: 200});
 
   expect(output).toEqual([
     [getObjWithSizePoop(23), getObjWithSizePoop(23)],
-    [getObjWithSizePoop(23)]
+    [getObjWithSizePoop(23)],
   ]);
 });
 
 it('should handle strings as well', () => {
-  const input = [
-    getObjWithSize(100), '{{{',
-    getObjWithSize(100)
-  ];
+  const input = [getObjWithSize(100), '{{{', getObjWithSize(100)];
   const output = chunkArray({input, bytesSize: 200});
 
-  expect(output).toEqual([
-    [getObjWithSize(100), '{{{'],
-    [getObjWithSize(100)]
-  ]);
+  expect(output).toEqual([[getObjWithSize(100), '{{{'], [getObjWithSize(100)]]);
 });
 
 it('should split 4 items into 2 chunks 1+1+1+1', () => {
-  const input = [
-    getObjWithSize(53),
-    getObjWithSize(89),
-    getObjWithSize(84),
-    getObjWithSize(33)
-  ];
+  const input = [getObjWithSize(53), getObjWithSize(89), getObjWithSize(84), getObjWithSize(33)];
   const output = chunkArray({input, bytesSize: 20});
 
   expect(output).toEqual([
     [getObjWithSize(53)],
     [getObjWithSize(89)],
     [getObjWithSize(84)],
-    [getObjWithSize(33)]
+    [getObjWithSize(33)],
   ]);
 });
 
 it('should split 12 very big items', () => {
   const input = [
-    getObjWithSize(120 * 1024), getObjWithSize(2 * 1024),
+    getObjWithSize(120 * 1024),
+    getObjWithSize(2 * 1024),
     getObjWithSize(200 * 1024),
-    getObjWithSize(33), getObjWithSize(44 * 1024), getObjWithSize(55 * 1024)
+    getObjWithSize(33),
+    getObjWithSize(44 * 1024),
+    getObjWithSize(55 * 1024),
   ];
   const output = chunkArray({input, bytesSize: 200 * 1024});
 
   expect(output).toEqual([
     [getObjWithSize(120 * 1024), getObjWithSize(2 * 1024)],
     [getObjWithSize(200 * 1024)],
-    [getObjWithSize(33), getObjWithSize(44 * 1024), getObjWithSize(55 * 1024)]
+    [getObjWithSize(33), getObjWithSize(44 * 1024), getObjWithSize(55 * 1024)],
   ]);
 });
 
 it('should split complex structure w/ empty array', () => {
   const input = [
-    getObjWithSize(128), getObjWithSize(128), getObjWithSize(128), getObjWithSize(128),
+    getObjWithSize(128),
+    getObjWithSize(128),
+    getObjWithSize(128),
+    getObjWithSize(128),
     getObjWithSize(128 * 10),
-    getObjWithSize(128), getObjWithSize(128), getObjWithSize(128), getObjWithSize(128),
+    getObjWithSize(128),
+    getObjWithSize(128),
+    getObjWithSize(128),
+    getObjWithSize(128),
     getObjWithSize(128 * 10),
-    getObjWithSize(12), getObjWithSize(12), getObjWithSize(12), getObjWithSize(12),
+    getObjWithSize(12),
+    getObjWithSize(12),
+    getObjWithSize(12),
+    getObjWithSize(12),
     getObjWithSize(1024),
-    {}, {}
+    {},
+    {},
   ];
   const output = chunkArray({input, bytesSize: 1024});
 
@@ -114,25 +108,38 @@ it('should split complex structure w/ empty array', () => {
     [getObjWithSize(128 * 10)],
     [getObjWithSize(12), getObjWithSize(12), getObjWithSize(12), getObjWithSize(12)],
     [getObjWithSize(1024)],
-    [{}, {}]
+    [{}, {}],
   ]);
-})
+});
 
 it('should split complex structure w/ empty array 2', () => {
-  const input = [getObjWithSize(25000), getObjWithSize(25000), getObjWithSize(25000), getObjWithSize(25000), getObjWithSize(25000), getObjWithSize(25000), getObjWithSize(25000), getObjWithSize(25000), getObjWithSize(25000), getObjWithSize(25000)];
+  const input = [
+    getObjWithSize(25000),
+    getObjWithSize(25000),
+    getObjWithSize(25000),
+    getObjWithSize(25000),
+    getObjWithSize(25000),
+    getObjWithSize(25000),
+    getObjWithSize(25000),
+    getObjWithSize(25000),
+    getObjWithSize(25000),
+    getObjWithSize(25000),
+  ];
   const output = chunkArray({input, bytesSize: 230000});
 
-  expect(output[0].length).toEqual(9);
+  expect(output[0]).toHaveLength(9);
 });
 
 it('should throw error in case of too big object if failOnOversize passed', () => {
   const input = [getObjWithSize(25000), getObjWithSize(250000), getObjWithSize(25000)];
+
   expect.assertions(1);
 
   try {
     chunkArray({input, bytesSize: 230000, failOnOversize: true});
-  } catch(e) {
-    expect(e.message).toEqual(`Can't chunk array as item is bigger than the max chunk size`)
+  } catch (e) {
+    // @ts-ignore
+    expect(e.message).toEqual(`Can't chunk array as item is bigger than the max chunk size`);
   }
 });
 
@@ -140,5 +147,6 @@ it('should not throw error in case of too big object by default', () => {
   const input = [getObjWithSize(25000), getObjWithSize(250000), getObjWithSize(25000)];
 
   const output = chunkArray({input, bytesSize: 230000});
-  expect(output[2].length).toEqual(1);
+
+  expect(output[2]).toHaveLength(1);
 });
